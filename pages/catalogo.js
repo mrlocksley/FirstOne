6e102ab33f31786359c278b3ddf4dea880373d40
mrlocksley <rafaelsena10@hotmail.com>
2025-02-06 13:49:08 -0300

    correção catalogo e sobre

---
+++ pages/catalogo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Catalogo = () => {
+  return (
+    <div>
+      <h1>Catálogo</h1>
+      <p>Bem-vindo ao catálogo!</p>
+    </div>
+  );
+}
+
+export default Catalogo;
---
+++ pages/sobre.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sobre = () => {
+  return (
+    <div>
+      <h1>Sobre</h1>
+      <p>Esta é a página sobre o projeto.</p>
+    </div>
+  );
+}
+
+export default Sobre;
